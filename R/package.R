2495a1124a722c20ecdda2724553ce4f42a64176
Isabelle Auger <isabelle.auger@mrnf.gouv.qc.ca>
2025-09-24 07:44:06 -0400

    Ménage dans Imports et Depends et ajout package.R

---
+++ R/package.R
@@ -0,0 +1,9 @@
+# Ce fichier sert à centraliser les imports du package
+#' @importFrom dplyr select filter left_join bind_cols group_by mutate summarise ungroup inner_join bind_rows
+#' @importFrom terra rast extract
+#' @importFrom magrittr %>%
+#' @importFrom sf st_crs st_as_sf st_transform
+#' @importFrom tidyr pivot_longer pivot_wider
+#' @importFrom stringr str_sub str_length
+#' @keywords internal
+"_PACKAGE"
